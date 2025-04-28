6d6c500deefde2fd1be629c3113ec4f09fa55ba4
weqpro <weqpro@gmail.com>
2025-04-28 14:26:42 +0300

    generating random chunks

--- src/backend/cells.py
+++ src/backend/cells.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from typing import Any
+
+
 class CellTypes(Enum):
     WATER = 0
     SAND = 1
@@ -6,21 +9,25 @@ class CellTypes(Enum):
     FOREST = 3
     PLATEAU = 4
 
-CELL_RANGES: dict[int, tuple[int, int]] = {
+
+CELL_RANGES: dict[Any, tuple[int, int]] = {
     CellTypes.WATER: (0, 7),
     CellTypes.SAND: (8, 14),
     CellTypes.GRASS: (15, 23),
     CellTypes.FOREST: (24, 28),
-    CellTypes.PLATEAU: (29, 36)    
+    CellTypes.PLATEAU: (29, 36),
 }
 
 MAX_RANGE = 36
 
+
 class Cell:
-    '''
+    """
     Class for cells
-    '''
+    """
+
     MAX_AGE = 10
-    def __init__(self, type_ = CellTypes.WATER, age = 0):
+
+    def __init__(self, type_=CellTypes.WATER, age=0):
         self.type_ = type_
         self.age = age
--- src/backend/evolution.py
+++ src/backend/evolution.py
@@ -1,12 +1,15 @@
 from numpy.typing import ArrayLike
 import numpy as np
+
+
 def evolve(bigger_chunk):
     CHUNK_SIZE = 16
     result = np.array((CHUNK_SIZE, CHUNK_SIZE), dtype=np.int8)
-    NEIGHBORS = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1) ,(0, -1)]
-    for i in range(1, CHUNK_SIZE-1):
-        for j in range(1, CHUNK_SIZE-1):
+    NEIGHBORS = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
+    for i in range(1, CHUNK_SIZE - 1):
+        for j in range(1, CHUNK_SIZE - 1):
             counter = sum(bigger_chunk[k][m].type_ for (k, m) in NEIGHBORS)
-                
+
 
 def generate_chunk(bigger_chunk): ...
+
