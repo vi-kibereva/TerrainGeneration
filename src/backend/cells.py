c9877a94ddc699366e9e392ba5dc639572332dda
Viktoriya Kibyeryeva <v.kibereva@gmail.com>
2025-04-29 20:54:02 +0300

    added basic evolution logic

--- src/backend/cells.py
+++ src/backend/cells.py
@@ -4,30 +4,15 @@ from typing import Any
 
 class CellTypes(Enum):
     WATER = 0
-    SAND = 1
-    GRASS = 2
-    FOREST = 3
-    PLATEAU = 4
+    LAND = 1
+    MOUNTAIN = 2
 
 
 CELL_RANGES: dict[Any, tuple[int, int]] = {
-    CellTypes.WATER: (0, 7),
-    CellTypes.SAND: (8, 14),
-    CellTypes.GRASS: (15, 23),
-    CellTypes.FOREST: (24, 28),
-    CellTypes.PLATEAU: (29, 36),
+    CellTypes.WATER: (0, 6),
+    CellTypes.LAND: (7, 12),
+    CellTypes.MOUNTAIN: (13, 18)
 }
 
 MAX_RANGE = 36
 
-
-class Cell:
-    """
-    Class for cells
-    """
-
-    MAX_AGE = 10
-
-    def __init__(self, type_=CellTypes.WATER, age=0):
-        self.type_ = type_
-        self.age = age
