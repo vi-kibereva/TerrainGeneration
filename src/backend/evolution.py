6c64133f34791712fc331b37f446ddeaeda97771
Viktoriya Kibyeryeva <v.kibereva@gmail.com>
2025-04-30 23:57:57 +0300

    added part of the biome generation logic

--- src/backend/evolution.py
+++ src/backend/evolution.py
@@ -1,9 +1,12 @@
 import numpy as np
 from scipy.signal import convolve2d
-from .cells import CELL_LUT
+# from .cells import CELL_LUT
+from numba import njit, prange
 
 NUMBER_OF_ITERATIONS = 10
 KERNEL = np.ones((3, 3), dtype=int)
+KERNEL2 = np.full((5, 5), 1/25)
+CHUNK_SIZE = 16
 
 
 def evolve(bigger_chunk: np.ndarray) -> None:
@@ -16,3 +19,24 @@ def generate_chunk(bigger_chunk: np.ndarray) -> np.ndarray:
     for _ in range(NUMBER_OF_ITERATIONS):
         evolve(bigger_chunk)
     return bigger_chunk[1:-1, 1:-1]
+
+def get_biome(terrain:int, value:np.ndarray) -> int:
+    return 1
+
+
+@njit(parallel=True, fastmath=True)
+def biome_evolution(bigger_chunk:np.ndarray) -> np.ndarray:
+    terrain = bigger_chunk & 0b11
+    biome = (bigger_chunk & 0b1100) >> 2
+    size = CHUNK_SIZE
+    result = np.zeros((size, size), dtype=np.int8)
+    for i in prange(size):
+        for j in range(size):
+            value = np.zeros(4, dtype=np.float32)
+            for ii in range(0, 5):
+                for jj in range(0, 5):
+                    # acc += bigger_chunk[i + ii, j + jj] * KERNEL2[5 - 1 - ii, 5 - 1 - jj]
+                    biome_idx = int(biome[i + ii, j + jj])
+                    value[biome_idx] += 1 * KERNEL2[ii, jj]
+            # result[i, j] = get_biome(terrain[i, j], value)
+    return result
