e2cbc44a2536f002c3a41cc2fccb4518d39e7b9e
Viktoriya Kibyeryeva <v.kibereva@gmail.com>
2025-05-01 16:07:47 +0300

    added get_biome function

--- src/backend/evolution.py
+++ src/backend/evolution.py
@@ -1,12 +1,20 @@
 import numpy as np
 from scipy.signal import convolve2d
-from .cells import CELL_LUT
+from .cells import CELL_LUT, CellTypes
 from numba import njit, prange
 
 NUMBER_OF_ITERATIONS = 10
-KERNEL = np.ones((3, 3), dtype=int)
-KERNEL2 = np.full((5, 5), 1/25)
+KERNEL = np.ones((3, 3), dtype=np.int8)
+BIOME_KERNEL = np.full((5, 5), 1/25, dtype=np.float32)
+BIOME_KERNEL_GAUSS = np.array([
+    [0.00390625, 0.015625  , 0.0234375 , 0.015625  , 0.00390625],
+    [0.015625  , 0.0625    , 0.09375   , 0.0625    , 0.015625  ],
+    [0.0234375 , 0.09375   , 0.140625  , 0.09375   , 0.0234375 ],
+    [0.015625  , 0.0625    , 0.09375   , 0.0625    , 0.015625  ],
+    [0.00390625, 0.015625  , 0.0234375 , 0.015625  , 0.00390625]
+])
 CHUNK_SIZE = 16
+DENSITY = 0.7
 
 
 def evolve(bigger_chunk: np.ndarray) -> None:
@@ -21,22 +29,32 @@ def generate_chunk(bigger_chunk: np.ndarray) -> np.ndarray:
     return bigger_chunk[1:-1, 1:-1]
 
 def get_biome(terrain:int, value:np.ndarray) -> int:
-    return 1
-
+    probs = value / value.sum()
+    biome = np.random.choice(4, p=probs)
+    return (biome << 2) | terrain
+    
 
 @njit(parallel=True, fastmath=True)
-def biome_evolution(bigger_chunk:np.ndarray) -> np.ndarray:
-    terrain = bigger_chunk & 0b11
+def biome_evolve(bigger_chunk:np.ndarray) -> np.ndarray:
+    terrain: np.ndarray = bigger_chunk & 0b11
     biome = (bigger_chunk & 0b1100) >> 2
     size = CHUNK_SIZE
     result = np.zeros((size, size), dtype=np.int8)
     for i in prange(size):
         for j in range(size):
             value = np.zeros(4, dtype=np.float32)
-            for ii in range(0, 5):
-                for jj in range(0, 5):
-                    # acc += bigger_chunk[i + ii, j + jj] * KERNEL2[5 - 1 - ii, 5 - 1 - jj]
-                    biome_idx = int(biome[i + ii, j + jj])
-                    value[biome_idx] += 1 * KERNEL2[ii, jj]
-            # result[i, j] = get_biome(terrain[i, j], value)
-    return result
+            for k in range(0, 5):
+                for m in range(0, 5):
+                    # acc += bigger_chunk[i + k, j + m] * BIOME_KERNEL[5 - 1 - k, 5 - 1 - m]
+                    if terrain[i, j] != terrain[i+k, j+k]:
+                        result[i, j] = biome[i, j]
+                    else:
+                        biome_idx: np.int8 = biome[i + k, j + m]
+                        value[biome_idx] += 1 * BIOME_KERNEL[k, m]
+    bigger_chunk[1:-1, 1:-1] = result
+
+def generate_chunk_biome(bigger_chunk:np.ndarray) -> np.ndarray:
+    for _ in range(NUMBER_OF_ITERATIONS):
+        biome_evolve(bigger_chunk)
+    return bigger_chunk
+
