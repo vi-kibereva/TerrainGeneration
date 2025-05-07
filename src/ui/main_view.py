d62f075507b0f1b277fc150a6725faec7a23e00e
Anastasia Liubenchuk <liubenchuk.pn@ucu.edu.ua>
2025-05-07 16:00:14 +0300

    Added zoom, map expansion when player moves, removed grid

--- src/ui/main_view.py
+++ src/ui/main_view.py
@@ -1,41 +1,40 @@
 from PySide6 import QtWidgets
 from src.backend.grid import Grid
-from src.ui.grid_view import ControlPanel, GridView
-
+from src.ui.grid_view import GridView
+from src.ui.panel_view import ControlPanel
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, grid):
         super().__init__()
         self.setWindowTitle("Terrain generation")
 
-        # Головний лейаут
         central = QtWidgets.QWidget()
         layout = QtWidgets.QHBoxLayout(central)
 
-        # Панель керування зліва
         self.control_panel = ControlPanel(parent=self)
         layout.addWidget(self.control_panel)
 
-        # Візуалізація грида справа
         self.grid_view = GridView(grid, parent=self)
         layout.addWidget(self.grid_view, stretch=1)
 
         self.setCentralWidget(central)
 
+    # Передаємо виклики від панелі до GridView
+    def generate_grid(self, seed, density):
+        self.grid_view.generate_grid(seed, density)
+
+    def clear_grid(self):
+        self.grid_view.clear_grid()
 
 
 def main():
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-
-    # Ініціалізуємо модель і створюємо сітку початково
     grid = Grid(density=0.5)
     grid.generate_around((0, 0), generated_radius=2, noise_radius=5)
 
-    # Створюємо головне вікно
     window = MainWindow(grid)
     window.resize(1000, 700)
     window.show()
-
     sys.exit(app.exec())
